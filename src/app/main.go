ca75d5ec58481414f28bb47a3e7f24c1fc66f061
Tim Havlicek <tim.h4vlicek@gmail.com>
2021-11-29 22:08:14 +0100

    go init

--- src/app/main.go
+++ src/app/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"context"
 	"io"
 	"log"
 	"net/http"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/polly"
 )
 
 func main() {
+	// Load the Shared AWS Configuration (~/.aws/config)
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := polly.NewFromConfig(cfg)
+
 	// Hello world, the web server
 
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
@@ -16,4 +28,10 @@ func main() {
 	http.HandleFunc("/hello", helloHandler)
 	log.Println("Listing for requests at http://localhost:8000/hello")
 	log.Fatal(http.ListenAndServe(":8000", nil))
+
+	// TODO: db with accounts and widgets
+	// TODO: api for dashboard
+	// TODO: auth api
+	// TODO: aws polly proxy
+	// TODO: Listen to event sub
 }
